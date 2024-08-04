524b00c0d57fd77b95d9c808880e0fdf2f370370
Alfin S. <benjaminstwo@gmail.com>
2024-08-04 22:16:09 +0800

    challenges

---
+++ challenges/plus_minus.py
@@ -0,0 +1,26 @@
+##############
+# Solution 1 #
+##############
+def plusMinus_1(arr):
+    """
+    O(n) - Linear Time
+    """
+    positive, negative, zero = (0, 0, 0)
+    for v in arr:
+        # find zero number:
+        if v == 0:
+            zero += 1
+        # find negative number
+        elif v < 0:
+            negative += 1
+        # find positive number
+        elif v > 0:
+            positive += 1
+
+    length = len(arr)
+    print(positive / length)
+    print(negative / length)
+    print(zero / length)
+
+def run():
+    plusMinus_1([-4, 3, -9, 0, 4, 1])
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import basic.lists as List
 import algorithm.big_o as BigO
+import challenges.plus_minus as ChallengePlusMinus
 
 if __name__ == "__main__":
     List.run()
     BigO.run()
+    ChallengePlusMinus.run()
