4dc89a61fb0c5f2ea0b84bb8e14a583edda9b429
Alfin S. <benjaminstwo@gmail.com>
2024-08-06 10:08:51 +0800

    find_first_non_repeating_char

--- challenges/strings.py
+++ challenges/strings.py
@@ -20,5 +20,33 @@ def reverse_string(text: str):
 
     return ''.join(arr)
 
+def find_first_non_repeating_char(text: str) -> str:
+    """
+    Input: aabccdeff
+    Output: 'b'
+
+    Input: aabbcc
+    Output: ''
+
+    Time complexity: O(n)
+    Space complexity: O(n)
+    """
+    found = ""
+    collect = {}
+    for t in text:
+        if t not in collect:
+            collect[t] = 0 # O(n)
+
+        if t in collect:
+            collect[t] += 1 # O(n)
+
+    for key, value in collect.items():
+        if value == 1:
+            found = key
+            break;
+
+    return found
+
 def run():
     print('reverse_string', reverse_string('how are you'))
+    print('find_first_non_repeating_char', find_first_non_repeating_char('aabbcc'))
